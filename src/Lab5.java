583e13bc00b93a3c196013cdffa31480d0e03543
Gabriel Indiongco <gindiongco1024@gmail.com>
2024-08-24 05:54:39 -0700

    First commit

---
+++ src/Lab5.java
@@ -0,0 +1,68 @@
+/*-------------------------------------------------------------
+// AUTHOR: your name.
+// FILENAME: title of the source file.
+// SPECIFICATION: your own description of the program.
+// FOR: CS 1400- Lab #5
+// TIME SPENT: how long it took you to complete the assignment.
+//-----------------------------------------------------------*/
+
+import java.util.Scanner;
+
+public class Lab5 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String sFirstname, sLastname, sBroncoId;
+        double sGrade;
+        String pName, pDesc, pFname;
+
+        // Read some input data
+        System.out.println("The student's first name: ");
+        sFirstname = scan.nextLine();
+        System.out.println("The student's alleged last name: ");
+        sLastname = scan.nextLine();
+        System.out.println("The student's Bronco Id: ");
+        sBroncoId = scan.nextLine();
+        System.out.println("Program name: ");
+        pName = scan.nextLine();
+        System.out.println("Program desc: ");
+        pDesc = scan.nextLine();
+        System.out.println("Program filename: ");
+        pFname = scan.nextLine();
+        System.out.println("Program grade: ");
+        sGrade = scan.nextDouble();
+        System.out.println();
+        scan.close();
+
+
+        // Create a Student object "student1"
+        // -->
+        Student student1 = new Student(sFirstname, sLastname, sBroncoId);
+
+        System.out.println("Making a student record\n...[" + student1 + "]");
+
+        // Use the setGrade setter to set student1's grade
+        // -->
+        student1.setGrade(sGrade);
+
+        // Create a Program object "program1" by "student1"
+        // -->
+        Program program1 = new Program(pName, pDesc, pFname, student1);
+        System.out.println("Making a program record\n...[" + program1 + "]");
+
+        // Produce a report
+        makeReport(program1);
+
+    }
+
+    public static void makeReport(Program program1) {
+        System.out.println("\n========== Program Submission Detail ==========");
+        System.out.println(String.format("%-12s: %-10s", "Student", program1.getAuthor().getFullName()));
+        System.out.println(String.format("%-12s: %-10s", "Bronco Id", program1.getAuthor().getBroncoId()));
+        System.out.println(String.format("%-12s: %-10s", "Grade", "" + program1.getAuthor().getGrade()));
+        System.out.println("");
+        System.out.println(String.format("%-12s: %-10s", "Program", program1.getProgramName()));
+        System.out.println(String.format("%-12s: %-10s", "Filename", program1.getFileName()));
+        System.out.println(String.format("%-12s: %-10s", "Description", program1.getDescription()));
+        System.out.println(String.format("%-12s: %-10s", "Datetime", program1.getCreatedDate()));
+    }
+}
---
+++ src/Student.java
@@ -0,0 +1,39 @@
+public class Student {
+    private String firstName;
+    private String lastName;
+    private String fullName;
+    private String broncoId;
+    private double grade;
+
+    public Student(String firstName, String lastName, String broncoId){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.broncoId = broncoId;
+        fullName = firstName + " " + lastName;
+        grade = -1;
+    }
+
+    public String getFirstName(){
+        return firstName;
+    }
+    public String getLastName(){
+        return lastName;
+    }
+    public String getFullName(){
+        return firstName + " " + lastName;
+    }
+    public String getBroncoId(){
+        return broncoId;
+    }
+    public double getGrade(){
+        return grade;
+    }
+
+    public String toString(){
+        return String.format("Student: %s %s, Bronco Id: %s, Grade: %.2f", firstName, lastName, broncoId, grade);
+    }
+
+    public void setGrade(double newGrade){
+        grade = newGrade;
+    }
+}
